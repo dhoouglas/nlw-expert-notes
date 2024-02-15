4872fe16f8a04cb68adba5cd56b94b6b9ce684aa
unknown <dhoouglas@hotmail.com>
2024-02-15 13:04:32 -0300

    Components NoteCard and NewNoteCard

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
@@ -16,41 +18,14 @@ export function App() {
       <div className="h-px bg-slate-700"/>
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
+        <NewNoteCard />
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden">
-          <span className="text-sm font-medium text-slate-300">
-            Há 2 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reiciendis quasi iste! Facilis voluptatibus ducimus repellat nesciunt consequatur deleniti ex ratione, quis animi cupiditate dolorem, eius explicabo rerum reprehenderit atque!
-          </p>
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-300">
-            Há 4 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore voluptatem sit hic voluptatum qui corporis, accusamus eius corrupti enim officia sequi tempora eos maiores quasi iste blanditiis quisquam aliquid expedita.
-          </p>
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-300">
-            Há 4 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore voluptatem sit hic voluptatum qui corporis, accusamus eius corrupti enim officia sequi tempora eos maiores quasi iste blanditiis quisquam aliquid expedita.
-          </p>
-        </div>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
     
